6e777570f219d810c83b4d7928c5d5e5ccf7eee3
Kenwen1 <kwen@sfsu.edu>
2024-10-25 16:38:39 -0700

    middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,12 @@
 import httpErrors from "http-errors";
 import express from "express";
+import { timeMiddleware } from "./middleware/time";
 import rootRoutes from "./routes/root";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(timeMiddleware);
 app.use("/", rootRoutes);
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
