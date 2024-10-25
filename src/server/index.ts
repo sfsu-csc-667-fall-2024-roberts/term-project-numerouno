df55149c65124d69dd0ea2851f987b19a8964640
Kenwen1 <kwen@sfsu.edu>
2024-10-25 16:28:58 -0700

    simplify app startup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,13 @@
+import httpErrors from "http-errors";
 import express from "express";
-
+import rootRoutes from "./routes/root";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get("/", (_request, response) => {
-    response.send("Hello World!");
-});
+app.use("/", rootRoutes);
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+console.log(`Server is running on port ${PORT}`);
 });
+app.use((_request, _response, next) => {
+    next(httpErrors(404));
+    });
